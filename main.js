e2b9bd903845cd248153b7d647bfc8cfda126e3b
abdurrahman-ra <abdurrahman.tantawi@gmail.com>
2023-05-11 15:30:50 +0300

    v1.2 code cleaning

--- main.js
+++ main.js
@@ -40,10 +40,16 @@ inputTextField.addEventListener("keydown", function (e) {
   if (e.key === "Enter") {
     let clickOnAddButton = new Event("click");
     addButton.dispatchEvent(clickOnAddButton);
-    // Prevent the "Enter" key itself from submitting the task (this will be done by "Add" button)
+
+    /* Prevent the "Enter" key itself from submitting the task
+    (this will be done by "Add" button) */
     e.preventDefault();
   }
 });
+
+/*_____________________________________________________________________________*/
+
+/* FUNCTIONS */
 
 // [5 - FUNCTION] Print All Existing Tasks On Page Load
 let printAllTasks = function () {
@@ -56,10 +62,12 @@ let printAllTasks = function () {
     let checkboxDiv = document.createElement("div");
 
     // [5.3] Create an unchecked checkbox for each output task
+
     // [5.3.1] The checkbox
     let outputTask = document.createElement("input");
     outputTask.type = "checkbox";
     outputTask.id = `task-${tasks.length - i}-checkbox`;
+
     // [5.3.2] Make it checked / unchecked based on what's saved in local storage
     outputTask.checked = checkOrder[i];
     outputTask.classList.add("output-task-text");
@@ -90,6 +98,7 @@ let printAllTasks = function () {
   // [5.7] Activate the delete button for each printed task
   tasksHTML = document.querySelectorAll(".output-task");
   activateDeleteButton(tasksHTML);
+
   // [5.8] Activate the checkbox button for each printed task
   activateCheckboxes(tasksHTML);
 };
@@ -100,19 +109,25 @@ let insertTask = function () {
   let userInput = inputTextField.value;
   if (userInput.length !== 0) {
     inputTextField.setAttribute("placeholder", "Add another task");
+
     // [6.2] Push it to an "tasks" array (descending order)
     tasks.unshift(userInput);
+
+    // [6.3] Similarly for the unchecked checkbox
     checkOrder.unshift(false);
-    // [6.3] Update the local storage
+
+    // [6.4] Update the local storage for both
     window.localStorage.tasks = JSON.stringify(tasks);
     window.localStorage.checkOrder = JSON.stringify(checkOrder);
-    // [6.4] Print the tasks on the document (Section 7)
+
+    // [6.5] Print the tasks on the document (Section 7)
     printTask();
-    // [6.5] Clear the input text field and focus again
+
+    // [6.6] Clear the input text field and focus again
     inputTextField.value = "";
     inputTextField.focus();
   } else {
-    // [6.6] For empty tasks, display a message to the user
+    // [6.7] For empty tasks, display a message to the user
     inputTextField.setAttribute("placeholder", "Can't add an empty task");
   }
 };
@@ -161,6 +176,8 @@ let printTask = function () {
 
   // [7.9] Activate the delete button for printed tasks
   activateDeleteButton(tasksHTML);
+
+  // [7.10] Activate the checkbox saving for printed tasks
   activateCheckboxes(tasksHTML);
 };
 
@@ -180,16 +197,15 @@ let activateDeleteButton = function (sectionsWithDelBtn) {
       // [8.2.2] Remove the value from the tasks array
       k = tasks.indexOf(taskToBeRm);
       tasks.splice(k, 1);
-      checkOrder.splice(k, 1);
-      console.log(checkOrder);
-      console.log(k);
-      // [8.2.2] Remove the value from the checks array
 
-      // [8.2.3] Update the local storage with the new arrays
+      // [8.2.3] Remove the value from the check order array
+      checkOrder.splice(k, 1);
+
+      // [8.2.4] Update the local storage with the new arrays
       window.localStorage.tasks = JSON.stringify(tasks);
       window.localStorage.checkOrder = JSON.stringify(checkOrder);
 
-      // [8.2.4] If the deleted task is the only one, remove the whole array from the local storage
+      // [8.2.5] If the deleted task is the last one, remove the whole array from the local storage
       if (tasks.length === 0) {
         window.localStorage.removeItem("tasks");
         window.localStorage.removeItem("checkOrder");
@@ -200,19 +216,25 @@ let activateDeleteButton = function (sectionsWithDelBtn) {
   // [8.3] If no tasks exist on webpage load, remove the array (if exists) from the local storage
   if (tasks.length === 0) {
     window.localStorage.removeItem("tasks");
+    window.localStorage.removeItem("checkOrder");
   }
 };
 
+// [9 - FUNCTION] Activate The Checkboxes for Inserted Tasks
 let activateCheckboxes = function (sectionsWithCheckbox) {
+  // [9.1] Initialization
   sectionsWithCheckbox.forEach(function (s, j) {
     let box = s.querySelector("div").querySelector(`input[type="checkbox"]`);
+
+    // [9.2] Checkboxes event trigger
     box.onclick = function () {
+      // [9.2.1] Update the checkOrder array
       checkOrder[j] = box.checked;
-      console.log(checkOrder);
-      console.log(j);
+      // [9.2.2] Update the local storage
       window.localStorage.checkOrder = JSON.stringify(checkOrder);
-      //window.localStorage.checkOrder = JSON.stringify(checkOrder);
     };
+
+    // [9.3] Update the local storage after clicking
     window.localStorage.checkOrder = JSON.stringify(checkOrder);
   });
 };
